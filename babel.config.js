752ebfcba710063cef513054431a29edde1e2ac5
lentoo <729533020@qq.com>
2019-01-30 11:26:38 +0800

    :sparkles: 打包 production 环境自动清除 console

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,26 @@
+const IS_PROD = process.env.NODE_ENV === "production";
+const plugins = [
+  [
+    'component',
+    {
+      'libraryName': 'element-ui',
+      'styleLibraryName': 'theme-chalk'
+    }
+  ]
+]
+
+if (IS_PROD) {
+  plugins.unshift([
+    "transform-remove-console",
+    {
+      exclude: ["error", "warn"]
+    }
+  ]);
+}
 module.exports = {
   presets: [
     // ['es2015', { 'modules': false }],
     '@vue/app'
   ],
-  plugins: [
-    [
-      'component',
-      {
-        'libraryName': 'element-ui',
-        'styleLibraryName': 'theme-chalk'
-      }
-    ]
-  ]
+  plugins
 }
